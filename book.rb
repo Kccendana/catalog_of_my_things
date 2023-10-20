4a2c7a85db5e3e991a15f21fb0225b428e175427
kcccendana <kc_telyn@yahoo.com>
2023-10-20 12:10:53 +0900

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,26 +1,27 @@
 require_relative 'inc_helper'
 
 class Book < Items
-  attr_accessor :cover_state
-  attr_reader :id
+  attr_accessor :cover_state, :label
+  attr_reader :id, :archived
 
-  def initialize(published_date, cover_state)
+  def initialize(label, published_date, cover_state, _archived: false)
     super(published_date)
     @cover_state = cover_state
+    @archived = can_be_archived?
+    @label = label
   end
 
   def to_hash
     { 'id' => @id,
-      'published_date' => @published_date,
+      'label' => @label,
+      'published_date' => @published_date.to_s,
       'cover_state' => @cover_state,
-      'can_be_archived' => @can_be_archived }
+      'can_be_archived' => @archived }
   end
 
   def self.from_json(json_str)
     data = JSON.parse(json_str)
-    new(BookParams.new(data['published_date'],
-                       data['cover_state'],
-                       data['can_be_archived']))
+    new(data['label'], data['published_date'], data['cover_state'])
   end
 
   def can_be_archived?
